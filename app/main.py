b41969c4d3e1db8398a2b5e6374eb2050e263834
yangjp22 <jinyang0604@gmail.com>
2023-03-31 23:58:08 -0400

    add Procfile

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
-from .config import settings
 from .database import engine
 from .routers import user, post, auth
 
@@ -9,10 +9,20 @@ from .routers import user, post, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(user.router, prefix="/users", tags=["Users"])
 app.include_router(post.router, prefix="/posts", tags=["Posts"])
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 
+
+# app.add_middleware()
 
 @app.get('/')
 async def root():
